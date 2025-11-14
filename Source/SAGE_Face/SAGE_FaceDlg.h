01bb1923bd0de9218d740225d412918874337f5e
J크 <wns1915@naver.com>
2025-11-14 14:24:40 +0900

    init: mfc dlg init

---
+++ Source/SAGE_Face/SAGE_FaceDlg.h
@@ -0,0 +1,34 @@
+﻿
+// SAGE_FaceDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CSAGEFaceDlg 대화 상자
+class CSAGEFaceDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CSAGEFaceDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SAGE_FACE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
